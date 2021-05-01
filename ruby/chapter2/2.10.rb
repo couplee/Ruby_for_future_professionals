3a6bf1edb1f3ef3fc0e4d9002db5cf7fdf7e8dfd
couplee <couplee888@gmail.com>
2021-05-01 02:51:28 +0000

    [Finish] 2.12.2 エイリアスメソッド

--- ruby/chapter2/2.10.rb
+++ ruby/chapter2/2.10.rb
@@ -70,13 +70,13 @@ greeting('japan')  #=> "こんにちは"
 
 
            # 💠 2.10.3 unless文
-Rubyにはifと反対の意味を持つunlessがある。条件式が偽になった時だけ処理を実行する。if文で否定の条件を書いてる時は、unless文に書き換えれる。
-ex) 以下はif文
+  Rubyにはifと反対の意味を持つunlessがある。条件式が偽になった時だけ処理を実行する。if文で否定の条件を書いてる時は、unless文に書き換えれる。
+  ex) 以下はif文
 status = 'error'
 if status != 'ok'
   '何か以上があります'
 end  #=> "何か異常があります"
-ex)上記のif文を下記のようにunlessに書き換えれる
+  ex)上記のif文を下記のようにunlessに書き換えれる
 status = 'ok'
 unless status == 'ok'
   '何か異常があります'
@@ -84,9 +84,9 @@ else
   '正常です'
 end  #=> "正常です"
 
-だがunless文にはif文のelsifのようなelsunlessという条件分技はない。
-またunlessはifと同じように、unlessの戻り値を直接変数に代入したり、修飾子として文の後ろに置いたり、thenを入れることができる
-ex) unlessの結果を変数に代入する
+  だがunless文にはif文のelsifのようなelsunlessという条件分技はない。
+  またunlessはifと同じように、unlessの戻り値を直接変数に代入したり、修飾子として文の後ろに置いたり、thenを入れることができる
+  ex) unlessの結果を変数に代入する
 status = 'error'
 
 message =   # unlessの結果を変数に代入する
@@ -97,24 +97,24 @@ message =   # unlessの結果を変数に代入する
   end
 message  #=> '何か異常があります'
 
-ex) unlessを修飾子として使う
+  ex) unlessを修飾子として使う
 '何か異常があります' unless status == 'ok'  #=> "何か異常があります"
 
-ex) thenを入れる
+  ex) thenを入れる
 status = 'error'
 unless status == 'ok' then
   '何か異常があります'
 end  #=> "何か異常があります"
 
 
-if+否定条件は、unless+肯定条件に書き直すことができるが、必ず書き直さないといけないわけではない。
-if文の方が読みやすい場合、if+否定条件のままにしても大丈夫
+  if+否定条件は、unless+肯定条件に書き直すことができるが、必ず書き直さないといけないわけではない。
+  if文の方が読みやすい場合、if+否定条件のままにしても大丈夫
 
 
 
     # 💠 2.10.4 case文
-複数の条件を指定する時は、elsifを重ねるよりもcase文で書いた方がシンプルになる。
-ex) case文の構文
+  複数の条件を指定する時は、elsifを重ねるよりもcase文で書いた方がシンプルになる。
+  ex) case文の構文
 case 対象のオブジェクトや式
 when 値1
   値1に一致する場合の処理
@@ -126,8 +126,8 @@ else
   どれにも一致しない場合の処理
 end
 
-2.5.3 if文の説明で使ったサンプルコードをcase文に書き直す場合
-ex) if文を使う場合
+  2.5.3 if文の説明で使ったサンプルコードをcase文に書き直す場合
+  ex) if文を使う場合
 country = 'italy'
 
 if country == 'japan'
@@ -139,7 +139,7 @@ elsif country == 'italy'
 else
   '???'
 end  #=> "ciao"
-ex) case文を使う場合
+  ex) case文を使う場合
 case country
 
 when 'japan'
@@ -153,8 +153,8 @@ else
 end  #=> "ciao"
 
 
-また、Rubyのcase文ではwhen節に複数の値を指定して、どれかに一致すれば処理を実行する、という条件分技を書くことができる
-ex) when節に複数の値を指定する
+  また、Rubyのcase文ではwhen節に複数の値を指定して、どれかに一致すれば処理を実行する、という条件分技を書くことができる
+  ex) when節に複数の値を指定する
 country = 'アメリカ'
 case country
 when 'japan', '日本'
@@ -168,7 +168,7 @@ else
 end  #=> "Hello"
 
 
-if文と同じように、case文も最後に評価された式を戻り値として返すから、case文の結果を変数に入れることができる
+  if文と同じように、case文も最後に評価された式を戻り値として返すから、case文の結果を変数に入れることができる
 country = 'italy'
 
 message =
@@ -186,7 +186,7 @@ message =
 message  #=> "ciao"
 
 
-when節の後ろにはthenを入れることができる。thenを入れると以下の式のようにwhen節とその条件が真だった場合の処理を1行で書けるが、使用頻度は低い
+  when節の後ろにはthenを入れることができる。thenを入れると以下の式のようにwhen節とその条件が真だった場合の処理を1行で書けるが、使用頻度は低い
 country = 'italy'
 
 case country
@@ -195,3 +195,102 @@ when 'us' then 'Hello'
 when 'italy' then 'ciao'
 else '???'
 end  #=> "ciao"
+
+
+          # 💠 2.10.5 条件演算子（三項演算子）
+  RubyではC言語と同じような?を使った条件分技(三項演算子)を使うことができる。下記は構文
+  式 ? 真だった場合の処理 : 偽だった場合の処理
+
+  ex) 2.5.3 if文の説明で下記のようなコード例を使った
+n = 11
+if n > 10
+  '10より大きい'
+else
+  '10以下'
+end  #=> "10より大きい"
+
+  上記のコードは条件演算子を使うと下記のように書き直すことができる
+n = 11
+n > 10 ? '10より大きい' : '10以下'  #=> "10より大きい"
+
+
+  下記のように、条件分技した結果を変数に代入することも可能
+n = 11
+message = n >10 ? '10より大きい' : '10以下'
+message  #=> "10より大きい"
+
+  ※️シンプルなif/else文の場合、条件演算子を使った方がスッキリ書ける場合がある。逆に複雑な条件文の場合、反対に読みづらくなることもあるから、コードの可読性を考慮しながら利用するようにする
+
+
+
+
+                 # 💠 2.11.2 ?で終わるメソッド
+  Rubyのメソッド名は?や!で終わらせることができる。?で終わるメソッドは慣習として真偽値を返すメソッドになってる。下記はRubyで最初から用意されてるメソッドの中から、いくつかピックアップ
+    ex) 空文字列はtrue、違う場合false
+''.empty?  #=> true
+'abc'.empty?  #=> false
+
+    ex) 引数の文字列が含まれてたらtrue、違う場合false
+'watch'.include?('at')  #=> true
+'watch'.include?('in')  #=> false
+
+    ex) 奇数ならtrue、偶数ならfalse
+1.odd?  #=> true
+2.odd?  #=> false
+
+    ex) 偶数ならtrue、奇数ならfalse
+1.even?  #=> false
+2.even?  #=> true
+
+    ex) オブジェクトがnilならtrue、違う場合false
+nil.nil?  #=> true
+'abc'.nil?  #=> false
+1.nil?  #=> false
+
+
+  ?で終わるメソッドは自分で定義することができる。真偽値を返す目的のメソッドであれば、?で終わらせるようにした方がいい
+ex) 3の倍数ならtrue、それ以外はfalseを返す
+def multiple_of_three?(n)
+  n % 3 == 0
+end
+
+multiple_of_three?(4)  #=> false
+multiple_of_three?(5)  #=> false
+multiple_of_three?(6)  #=> true
+
+
+
+                   💠 2.11.3 !で終わるメソッド
+  !で終わるメソッドは慣習として「使用する時は注意が必要」という意味を持つ。
+  例えば、Stringクラスにはupcaseメソッドとupcase!メソッドという2つのメソッドがある。
+  どっちも文字列を大文字にするメソッドだけど、upcaseメソッドは"大文字に変えた新しい文字列を返して、呼び出した文字列自身は変化しない"。
+  だが、upcase!メソッドは"呼び出した文字列自身を大文字に変更する"
+    ex)
+a = 'ruby'
+
+a.upcase  #=>"RUBY"
+a  #=> "ruby"  # upcaseだと変数aの値は変化しない
+
+a.upcase!  #=> "RUBY"
+a  #=> "RUBY"  # upcase!だと変数aの値も大文字に変わる
+
+
+  ?で終わるメソッドと同様、!で終わるメソッドも自分で定義することができる。
+    ex) 引数として渡された文字列を逆順に並び替えて、さらに大文字に変更する破壊的メソッド(約14行下で説明)を定義してる
+def reverse_upcase!(s)
+  s.reverse!.upcase!
+end
+s = 'ruby'
+reverse_upcase!(s)  #=> "YBUR"
+s  #=> "YBUR"
+
+
+  メソッド名は!や?で終わることができるが、変数名には!や?を使えない（エラーになる）
+odd? = 1.odd?  #=> SyntaxError
+upcase! = 'ruby'.upcase!  #=> SyntaxError
+
+
+  upcase!メソッドみたいに、呼び出したオブジェクトの状態を変更してしまうメソッドのことを「破壊的メソッド」と呼ぶ
+  !で終わるメソッドは?で終わるメソッドほど慣習が明確ではない。破壊的メソッドがすべて!で終わるわけではないし、
+  破壊的メソッドではないメソッド(たとえば不正な状態で呼び出すとエラーを発生させるメソッドなど)に!をつける時もある。
+  いずれにしても!で終わるメソッドには何かしらの注意事項があるはずだから、!で終わるメソッドを見かけたらどんな注意事項があるか、APIドキュメントなどを読んで確認する。
