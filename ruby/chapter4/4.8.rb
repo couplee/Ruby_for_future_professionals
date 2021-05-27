163d9c4b5774dfdee61f8a6564a55b1c20c04f60
couplee <couplee888@gmail.com>
2021-05-27 14:37:27 +0000

    4.8.4

--- ruby/chapter4/4.8.rb
+++ ruby/chapter4/4.8.rb
@@ -31,3 +31,35 @@ fruits.map  #=> #<Enumerator: ["apple", "orange", "melon"]:map>
 fruits.delete_if  #=> #<Enumerator: ["apple", "orange", "melon"]:delete_if> 
 
   だからwith_indexメソッドはさまざまな繰り返し処理用のメソッドと組み合わせて実行できるように”見える”
+
+
+
+                            💠 4.8.3 添え字を0以外の数値から開始させる
+  eachメソッドやwith_indexメソッドを使ったら、繰り返し処理中に添え字が取得できて便利だが、添え字はいつも0から始まる。
+  0以外の数値(たとえば1や10)から始めたい場合、with_indexメソッドに引数を渡すと添え字が引数で渡した数値から開始する
+fruits = ['apple', 'orange', 'melon']
+
+fruits.each.with_index(1) { |fruit, i| puts "#{i}: #{fruit}" }  # eachで繰り返しながら、1から始まる添え字を取得する
+#=> 1: apple
+#   2: orange
+#   3: melon
+
+
+fruits.map.with_index(10) { |fruit, i| "#{i}: #{fruit}" }  # mapで処理しながら、10から始まる添え字を取得する
+#=> ["10: apple", "11: orange", "12: melon"]
+
+  each_with_indexメソッドには引数を渡せないから、each_with_index(1)ではなく、上のコードみたいにeach_with_index(1)の形で呼び出す必要がある
+
+
+
+                            💠 4.8.4 配列がブロック引数に渡される場合
+  配列の配列に対して繰り返し処理を実行したら、ブロック引数に配列が渡ってくる。
+  たとえば、縦の長さと横の長さを配列に格納して、それを複数用意した配列があったとする
+dimensions = [
+# [縦, 横]
+  [10, 20],
+  [30, 40],
+  [50, 60]
+]
+
+  これをeachメソッドなどで繰り返し処理したら、配列がブロック引数に渡ってくる
