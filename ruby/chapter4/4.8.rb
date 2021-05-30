6f71ae6bfe34e1aa5a92b374e119584d3ccd94d4
couplee <couplee888@gmail.com>
2021-05-30 13:55:06 +0000

    [Finish] 4.8.4

--- ruby/chapter4/4.8.rb
+++ ruby/chapter4/4.8.rb
@@ -63,3 +63,104 @@ dimensions = [
 ]
 
   これをeachメソッドなどで繰り返し処理したら、配列がブロック引数に渡ってくる
+dimensions = [
+# [縦, 横]
+  [10, 20],
+  [30, 40],
+  [50, 60]
+]
+
+areas = []  # 面積の計算結果を格納する配列
+
+dimensions.each do |dimension|  # ブロック引数が1個だったら、ブロック引数の値が配列になる
+  length = dimension[0]
+  width = dimension[1]
+  areas << length * width
+end
+
+areas  #=> [200, 1200, 3000]
+
+  だが、ブロック引数の数を2個にしたら、縦と横の値を別々に受け取ることができて、上のコードよりもシンプルに書ける
+dimensions = [
+# [縦, 横]
+  [10, 20],
+  [30, 40],
+  [50, 60]
+]
+
+areas = []  # 面積の計算結果を格納する配列
+
+dimensions.each do |length, width|  # 配列の要素分だけブロック引数を用意したら、各要素の値が別々の変数に格納される
+  areas << length * width
+end
+
+areas  #=> [200, 1200, 3000]
+
+  あんまり意味ないけど、ブロック引数が多すぎる場合、はみ出してるブロック引数はnilになる
+dimensions.each do |length, width, foo, bar|  # lengthとwidthには値が渡されるけど、fooとbarはnilになる
+  p [length, width, foo, bar]
+end
+#=> [10, 20, nil, nil]
+#   [30, 40, nil, nil]
+#   [50, 60, nil, nil]
+
+  配列の要素が3個あるのに、ブロック引数が2個しかない場合、3つ目の値が捨てられるが、わかりづらいから特別な理由がない限りこうしたコードを書かないように！
+dimensions = [
+  [10, 20, 100],
+  [30, 40, 200],
+  [50, 60, 300]
+]
+
+dimensions.each do |length, width|  # 3つの値をブロック引数に渡そうとするが、2つしかないから3つ目の値は捨てられる
+  p [length, width]
+end
+#=> [10, 20]
+#   [30, 40]
+#   [50, 60]
+
+  ではeach_with_indexみたいに、もとからブロック引数を2つ受け取る場合はどうするか、ためしに|length, width, i|みたいに3つのブロック引数を並べてみる
+dimensions = [
+  [10, 20],
+  [30, 40],
+  [50, 60]
+]
+dimensions.each_with_index do |length, width, i|
+  puts "length: #{length}, width: #{width}, i: #{i}"
+end
+#=> length: [10, 20], width: 0, i:
+#   length: [30, 40], width: 1, i:
+#   length: [50, 60], width: 2, i:
+
+  結果は、最初のブロック引数lengthに配列が丸ごと渡されてしまった。その影響でブロック引数の割り当てがずれて、widthに添え字が、iにnilが入ってる。
+  だから、下記のようにブロック引数を2つにしたら、第1引数で縦と横の値を配列として取得できる
+dimensions = [
+  [10, 20],
+  [30, 40],
+  [50, 60]
+]
+
+dimensions.each_with_index do |dimension, i|  # いったん配列のまま受け取る
+  length = dimension[0]  # 配列から縦と横の値を取り出す
+  width = dimension[1]
+  puts "length: #{length}, width: #{width}, i: #{i}"
+end
+#=> length: 10, width: 20, i: 0
+#=> length: 30, width: 40, i: 1
+#=> length: 50, width: 60, i: 2
+
+  だが一度配列で受け取ってから変数に入れ直すのが面倒。一度でブロック引数で受け取る方法がある。
+  下記のように配列の要素を受け取るブロック引数を()で囲んだら、配列の要素を別々の引数として受け取れる
+dimensions = [
+  [10, 20],
+  [30, 40],
+  [50, 60]
+]
+
+dimensions.each_with_index do |(length, width), i|
+  puts "length: #{length}, width: #{width}, i: #{i}"
+end
+#=> length: 10, width: 20, i: 0
+#=> length: 30, width: 40, i: 1
+#=> length: 50, width: 60, i: 2
+
+  これで縦の値、横の値、添え字の3つの値を一度にブロック引数で受け取れる
